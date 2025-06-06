d3ce454716ca0b0dd3ff7fc732dcc577979b8324
Alfiya <al.yausheva@gmail.com>
2025-06-06 13:09:51 +0300

    initialisation of project

---
+++ Notebook/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Textarea } from './components/Textarea'
+import NoteItem from './components/NoteItem'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+     
+    </>
+  )
+}
+
+export default App
